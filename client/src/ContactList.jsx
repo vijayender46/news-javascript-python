406cf29fd0fddebd47466f59723de487163ba20f
vj146 <121240553+vj146@users.noreply.github.com>
2024-05-01 23:04:33 +0100

    first commit

---
+++ client/src/ContactList.jsx
@@ -0,0 +1,56 @@
+
+import PropTypes from "prop-types";
+
+ContactList.propTypes = {
+    contacts: PropTypes.any,
+    updateContact: PropTypes.any,
+    updateCallback: PropTypes.any
+  };
+
+export default function ContactList(props) {
+    const {contacts, updateContact, updateCallback } = props;
+    const onDelete = async (id) => {
+        try {
+            const options = {
+                method: "DELETE"
+            }
+            const response = await fetch(`http://127.0.0.1:5000/delete_contact/${id}`, options)
+            if (response.status === 200) {
+                updateCallback()
+            } else {
+                console.error("Failed to delete")
+            }
+        } catch (error) {
+            alert(error)
+        }
+    }
+  
+    return (
+    <div>
+        <h2>contacts</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map((contact) => (
+                    <tr key={contact.id}>
+                        <td>{contact.firstName}</td>
+                        <td>{contact.lastName}</td>
+                        <td>{contact.email}</td>
+                            <td>
+                                <button onClick={() => updateContact(contact)}>Update</button>
+                                <button onClick={() => onDelete(contact.id)}>Delete</button>
+                            </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+  )
+}
